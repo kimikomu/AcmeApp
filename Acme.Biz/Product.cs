eb994c5c4fc2dc08cd116a915f028638b60bca7d
Kimiko <kimikomu@gmail.com>
2019-01-03 17:05:32 -0500

    Create Product class and unit tests.

---
+++ Acme.Biz/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>s
+    /// Manages products carried in inventory
+    /// </summary>
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; } 
+        public int ProductId { get; set; }
+
+        public Product()
+        {
+            Console.WriteLine("Product instance created.");
+        }
+
+        public Product(string productName, string description, int productId): this()
+        {
+            ProductName = productName;
+            Description = description;
+            ProductId = productId;
+            Console.WriteLine($"Product instance has a name: {ProductName}.");
+        }
+
+        public string SayHello()
+        {
+            return $"Hello {ProductName} ({ProductId}): {Description}";
+        }
+
+    }
+}
---
+++ Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Hammer";
+            currentProduct.ProductId = 1;
+            currentProduct.Description = "Smash with this!";
+            var expected = "Hello Hammer (1): Smash with this!";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SayHelloTest_ParameterizedContructor()
+        {
+            //Arrange
+            var currentProduct = new Product("Hammer", "Smash with this!", 1);
+            var expected = "Hello Hammer (1): Smash with this!";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
